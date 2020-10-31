f125aae18815e52c61f0d6d38cf082846b52f6ec
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-10-30 20:15:05 -0400

    remove Gaussian blur

--- tile_script.py
+++ tile_script.py
@@ -22,23 +22,23 @@ while True:
 
         frame = imutils.resize(frame, width=500)
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        frame = cv2.GaussianBlur(frame, (9,9), 0)
+        # frame = cv2.GaussianBlur(frame, (9,9), 0)
         (thresh, frame) = cv2.threshold(frame, 150, 255, cv2.THRESH_BINARY)
         (H, W) = frame.shape[:2]
         # print (frame, frame.shape)
         contours, hierarchy = cv2.findContours(frame.astype('uint8'), cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
         
         for idx, contour in enumerate(contours):
-            points = [contour[0]]
+            # points = [contour[0]]
             for i in range(1,len(contour)-1):
-                if abs(contour[i][0][0] - contour[i+1][0][0]) > 7 or abs(contour[i][0][1] - contour[i+1][0][1]):
-                    points.append(contour[i])
+                # if abs(contour[i][0][0] - contour[i+1][0][0]) > 7 or abs(contour[i][0][1] - contour[i+1][0][1]):
+                #     points.append(contour[i])
                 # a high value indicates the contour contains mostly white, so draw the contour (I used the boundingRect)
                 # if mean_val[0] < 100:
                 
                 cv2.rectangle(frame, tuple(contour[0][0]), tuple(contour[2][0]), (100,100,100), 10)
         
-            print (points)
+            # print (points)
     # for idx, contour in enumerate(contours):
         # if cv2.contourArea(contour) > 100:
         #     ma
