02dce69127e67f623432e23695f5c26e0f78b325
Srihari Vishnu <srihari.vishnu@gmail.com>
2020-11-07 01:31:43 -0500

    Working beta

--- tile_script.py
+++ tile_script.py
@@ -1,15 +1,13 @@
 import cv2
 import imutils
 from imutils.video import VideoStream
-from imutils.video import FPS
 import numpy as np
 import time
 
 initBox = None
 
-vs = cv2.VideoCapture("video.MP4")
+vs = cv2.VideoCapture("test_vid.MP4")
 
-fps = None
 paused= False
 frame = None
 while True:
@@ -21,30 +19,32 @@ while True:
             break
 
         frame = imutils.resize(frame, width=500)
+        (H, W) = frame.shape[:2]
+        frame = frame[H//10:4*H//5 - H//5, :]
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # frame = cv2.GaussianBlur(frame, (9,9), 0)
-        (thresh, frame) = cv2.threshold(frame, 150, 255, cv2.THRESH_BINARY)
-        (H, W) = frame.shape[:2]
+        (thresh, frame) = cv2.threshold(frame, 100, 255, cv2.THRESH_BINARY)
         # print (frame, frame.shape)
-        contours, hierarchy = cv2.findContours(frame.astype('uint8'), cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-        
-        for idx, contour in enumerate(contours):
+        # contours, hierarchy = cv2.findContours(frame.astype('uint8'), cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+        i = 0
+        for x in range(W//8, W, W//4):
+            i += 1
+            if (frame[-100][x] == 0):
+                print ("TAP: " + str(i))
+        # for idx, contour in enumerate(contours):
             # points = [contour[0]]
-            for i in range(1,len(contour)-1):
-                # if abs(contour[i][0][0] - contour[i+1][0][0]) > 7 or abs(contour[i][0][1] - contour[i+1][0][1]):
-                #     points.append(contour[i])
-                # a high value indicates the contour contains mostly white, so draw the contour (I used the boundingRect)
-                # if mean_val[0] < 100:
-                
-                cv2.rectangle(frame, tuple(contour[0][0]), tuple(contour[2][0]), (100,100,100), 10)
-        
+            # for i in range(1,len(contour)-1):
+            #     # if abs(contour[i][0][0] - contour[i+1][0][0]) > 7 or abs(contour[i][0][1] - contour[i+1][0][1]):
+            #     #     points.append(contour[i])
+            #     # a high value indicates the contour contains mostly white, so draw the contour (I used the boundingRect)
+            #     # if mean_val[0] < 100:
+            # if 3 <= len(contour) <= 4:
+            #     print (tuple(contour[0][0]), tuple(contour[2][0]))
+            #     cv2.rectangle(frame, tuple(contour[0][0]), tuple(contour[2][0]), (100,100,100), 10)
+        # print ("________")
             # print (points)
-    # for idx, contour in enumerate(contours):
-        # if cv2.contourArea(contour) > 100:
-        #     ma
-        # print (contour)
     # cv2.imshow("Frame", frame[H//3:, :])
-    cv2.imshow("Frame", frame[H//3:, :])
+    cv2.imshow("Frame", frame)
     
 
     key = cv2.waitKey(1) & 0xFF
